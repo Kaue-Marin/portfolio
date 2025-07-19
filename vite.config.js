83e848bbb7e49ddefd8134e8e967eef5f07e92f3
Kaue-Marin <kauezudo123@gmail.com>
2025-07-19 13:15:46 -0300

    arrumando vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio/', 
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  preview: {
+    port: parseInt(process.env.PORT) || 4173,
+    host: '0.0.0.0'
+  }
+})
