74a978b646eaf478bca5cf22ca6ca5291b1c45d2
Kaue-Marin <kauezudo123@gmail.com>
2025-07-19 13:18:15 -0300

    allow render host

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
+    host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 4173,
-    host: '0.0.0.0'
+    allowedHosts: ['portfolio-kaue.onrender.com']
   }
 })
